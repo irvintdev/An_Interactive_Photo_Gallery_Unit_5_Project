d0ff5beb1df9ed60781ebee630c3144231dacc5f
Irvin Torres <irvin7torres7@gmail.com>
2022-03-22 20:15:15 -0700

    Update myOwnSearchFilter.js
    added some notes

--- photo_gallery_v5/js/myOwnSearchFilter.js
+++ photo_gallery_v5/js/myOwnSearchFilter.js
@@ -2,16 +2,16 @@
 
 
 function myOwnSearchFilter() {
-    let input = document.getElementById("search");
-    input = input.value.toLowerCase();
-    const anchors = document.querySelectorAll('a');
+    let input = document.getElementById("search");  // THIS RETRIEVES INPUT FROM SEARCH BAR
+    input = input.value.toLowerCase();              //  AND THEN CONVERTS THE VALUE TO ALL LOWER CASE
+    const anchors = document.querySelectorAll('a'); // THIS SELECTS ALL ANCHORS IN HTML
 
-    for (let i=0; i < anchors.length; i++) {
-        let captions = anchors[i].getAttribute('data-caption');
-        if (captions.toLowerCase().includes(input)) {
-            anchors[i].style.display = "inherit";
+    for (let i=0; i < anchors.length; i++) {        // THIS WILL LOOP THROUGH ALL THE ANCHORS
+        let captions = anchors[i].getAttribute('data-caption');  // THIS RETRIEVES THE DATA CAPTION IN SELECTED ANCHOR
+        if (captions.toLowerCase().includes(input)) {   // THIS CONDITION CHECKS IF INPUT IS INCLUDED IN THE DATA CAPTION
+            anchors[i].style.display = "inherit";   // IF TRUE, THE ANCHOR WILL DISPLAY AS NORMAL
         } else {
-            anchors[i].style.display = "none";
+            anchors[i].style.display = "none";  // IF FALSE, THE ANCHOR WILL NOT DISPLAY
         }
     }
 }
