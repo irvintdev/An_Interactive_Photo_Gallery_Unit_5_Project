434bfb73cfad1106c20a5b01010adefb0fa68c1e
Irvin Torres <irvin7torres7@gmail.com>
2022-03-22 20:00:09 -0700

    created my own search filer :)

--- photo_gallery_v5/js/myOwnSearchFilter.js
+++ photo_gallery_v5/js/myOwnSearchFilter.js
@@ -1,15 +1,17 @@
 
-document.getElementById('search').addEventListener("keyup" , myOwnSearchFilter);
+
 
 function myOwnSearchFilter() {
     let input = document.getElementById("search");
-    input.value = input.value.toLowerCase();
-    console.log(input.value);
-}
+    input = input.value.toLowerCase();
+    const anchors = document.querySelectorAll('a');
 
-for ( let i = 1 ; i <= 12; i++) {
-    let anchorElements = document.getElementById(`picture${i}`);
-    let value = anchorElements.getAttribute('data-caption');
-    console.log(anchorElements);
-    console.log(value);
+    for (let i=0; i < anchors.length; i++) {
+        let captions = anchors[i].getAttribute('data-caption');
+        if (captions.toLowerCase().includes(input)) {
+            anchors[i].style.display = "inherit";
+        } else {
+            anchors[i].style.display = "none";
+        }
+    }
 }
